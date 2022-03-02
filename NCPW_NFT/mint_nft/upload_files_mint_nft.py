cddd134f8d56c42a63489c6ee74b380ca540a26d
YE JOON LEE <35551019+yejoonlee@users.noreply.github.com>
2022-03-02 14:20:30 +0900

    nft auto mint

--- NCPW_NFT/mint_nft/upload_files_mint_nft.py
+++ NCPW_NFT/mint_nft/upload_files_mint_nft.py
@@ -5,13 +5,14 @@ import PinataPy
 
 pinataPy = PinataPy.PinataPy()
 
+# os.chdir('/Users/yeznable/NCPW/nft')
+
 root_path = '/Users/yeznable/NCPW/'
 dir_path = root_path + 'nft-img/'
 paths_to_file = glob(dir_path + '*.png')
 res = pinataPy.pin_file_to_ipfs(paths_to_file=paths_to_file)
 print(res)
 IpfsHash = res['IpfsHash']
-# IpfsHash = 'QmbpntFEWee1hGu32F43Bg15dxjt52xuRpyBcpTSo4NMzj'
 
 for file in paths_to_file:
     file_name = file[len(dir_path):]
@@ -20,7 +21,7 @@ for file in paths_to_file:
     item_name = file_name.split('_')[0]
     style = file_name.split('_')[1].split('.')[0]
     metadata = {
-        "image": f"https://gateway.pinata.cloud/ipfs/{IpfsHash}{dir_path}{file_name}",
+        "image": f"https://gateway.pinata.cloud/ipfs/{IpfsHash}{dir_path[6:]}{file_name}",
         "tokenId": tokenId,
         "name": f"NCPW {tokenId}",
         "attributes": [
@@ -47,9 +48,8 @@ paths_to_file = glob(dir_path + '*.json')
 res = pinataPy.pin_file_to_ipfs(paths_to_file=paths_to_file)
 print(res)
 IpfsHash = res['IpfsHash']
-# IpfsHash = 'QmbpntFEWee1hGu32F43Bg15dxjt52xuRpyBcpTSo4NMzj'
 
-metadatas = ['']
+metadatas = []
 for file in paths_to_file:
     file_name = file[len(dir_path):]
     metadatas.append(file_name)
@@ -58,24 +58,21 @@ for file in paths_to_file:
     os.system(command)
     # print(command)
 
-metadata_root = f"https://gateway.pinata.cloud/ipfs/{IpfsHash}{dir_path}"
-mint_nft_links = \
-f"""
-//step 5: Call the mintNFT function
-const metadatalinks = {[metadata_root + f for f in metadatas]}
-"""\
-+\
-"""
-for (const s of metadatalinks) {
-  mintNFT(s)
-};
-"""
+metadata_root = f"https://gateway.pinata.cloud/ipfs/{IpfsHash}{dir_path[6:]}"
+os.chdir('/Users/yeznable/NCPW/nft')
 
-with open('/Users/yeznable/NCPW/nft/scripts/mint-nft.js', 'r') as mint_nft_js:
-    mint_nft = mint_nft_js.read().split('//step 5: Call the mintNFT function')[0]
+for f in metadatas:
+    mint_nft_links = \
+    f"""//step 5: Call the mintNFT function
+mintNFT("{metadata_root + f}")
+    """
+    print(mint_nft_links)
 
-with open('/Users/yeznable/NCPW/nft/scripts/mint-nft.js', 'w') as mint_nft_js:
-    mint_nft = mint_nft + mint_nft_links
-    mint_nft_js.write(mint_nft)
+    with open('/Users/yeznable/NCPW/nft/scripts/mint-nft.js', 'r') as mint_nft_js:
+        mint_nft = mint_nft_js.read().split('//step 5: Call the mintNFT function')[0]
 
-print(os.system('node /Users/yeznable/NCPW/nft/scripts/mint-nft.js'))
+    with open('/Users/yeznable/NCPW/nft/scripts/mint-nft.js', 'w') as mint_nft_js:
+        mint_nft = mint_nft + mint_nft_links
+        mint_nft_js.write(mint_nft)
+
+    os.system('node scripts/mint-nft.js')
